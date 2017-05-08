eb9c194d4733de545a6b458fb825cf71d73612d8
tachyla <tachylad.murray@gmail.com>
2017-05-07 21:00:15 -0700

    test/ship_test.js

--- test/ship_test.js
+++ test/ship_test.js
@@ -3,37 +3,36 @@ const should = require('chai').should();
 //CHECK FOR SHIP TEST SUITE
 describe('checkForShip', function () {
   let checkForShip = require('../game_logic/ship_methods').checkForShip;
-//SPEC: that tests no ship present at given coordinate
-  it('should correctly report no ship at a given players coordinate', function () {
+  let player;
+
+//BEFORE will create this object before each "checkFor Ship" SPEC runs;
+//its the most complex version of the obj that the test uses-> 1 player, many ships, many coordinates
+  before(function () {
     player = {
       ships: [
-             { locations: [[0, 0]] }
+                  { locations: [[0, 0], [0,1]] },
+                  { locations: [[1, 0], [1,1]] },
+                  { locations: [[0, 0], [0,1], [2,2], [2,4] ]}
       ]
-    };
+    }; 
+  });
+
+//SPEC: that tests no ship present at given coordinate
+  it('should correctly report no ship at a given players coordinate', function () {
+
     checkForShip(player, [9, 9]).should.be.false;
   });
 
 //SPEC: that tests ship present at given coordinates
   it('should correctly report a ship located at the given coordinates', function () {
-    player = {
-      ships: [
-             { locations: [[0, 0]] }
-      ]
-    };
+
     checkForShip(player, [0, 0]).should.deep.equal(player.ships[0]);
     //ship = player.ships[0]--> this is what checkForShips now retruns instead of 'true'
   });
 
 //SPEC: that tests for ships with more than one coordinate
   it('should handle ships with more than one coordinate', function () {
-    player = {
-      ships: [
-             { locations: [[0, 0], [0,1]] },
-             { locations: [[1, 0], [1,1]] },
-             { locations: [[0, 0], [0,1], [2,2], [2,4] ]}
-             
-      ]
-    };
+
     checkForShip(player, [0,1]).should.deep.equal(player.ships[0]);
     checkForShip(player, [0,0]).should.deep.equal(player.ships[0]);
     checkForShip(player, [1,0]).should.deep.equal(player.ships[1]);
@@ -44,12 +43,7 @@ describe('checkForShip', function () {
 
 //SPEC: that tests for multiple ships at given coordinates
   it('should handle checking multiple ships', function () {
-    player = {
-      ships: [
-             { locations: [[0, 0], [0,1]] },
-             { locations: [[1, 0], [1,1]] }
-      ]
-    };
+   
     //ship 1
     checkForShip(player, [0,1]).should.deep.equal(player.ships[0]);
     checkForShip(player, [0,0]).should.deep.equal(player.ships[0]);
@@ -63,7 +57,7 @@ describe('checkForShip', function () {
   });
 
 //DAMAGE TEST SUITE
-  describe('damageShip', function() {
+describe('damageShip', function() {
     const damageShip = require('../game_logic/ship_methods').damageShip;
     //SPEC: thats tests for damage to a ship at a given coordinate
     it('should register damage on a given ship at a given location', function() {
@@ -79,32 +73,32 @@ describe('checkForShip', function () {
 
 //FIRE TEST SUITE
   describe('fire', function() {
-      var fire = require('../game_logic/ship_methods').fire;
+    var fire = require('../game_logic/ship_methods').fire;
+    var player; 
+
+    before(function () {
+      player = {
+        ships: [
+                      { locations: [[0, 0]], 
+                        damage: [] }
+        ]
+      };
+    });
+
       //SPEC: that tests for damage on a players ship at a given coordinate
-      it('should record damage on the given players ship at a given coordinate', function() {
-          let player = {
-              ships: [
-                  { locations: [[0, 0]], damage: [] }
-                  ]
-                };
+      it('should record damage on the given players ship at a given coordinate', function() {    
 
-                fire(player, [0,0]);
+      fire(player, [0,0]);
 
-                (player.ships[0].damage[0]).should.deep.equal([0,0]);
-          });
+      (player.ships[0].damage[0]).should.deep.equal([0,0]);
+    });
           
-      //Should not record damage if there are no coordinates    
-      it('should NOT record damage if there is no ship at a given coordinates', function() {
-          let player = {
-              ships: [
-                  { locations: [[0, 0]], damage: [] }
-                  ]
-                };
+      //SPEC: that tests no damage recorded if no ship at given coordinates      
+    it('should NOT record damage if there is no ship at given coordinates', function() {
 
-                fire(player, [9,9]);
+      fire(player, [9,9]);
 
-                (player.ships[0].damage).should.be.empty;
-          });
+      (player.ships[0].damage).should.not.deep.equal([0,0]);
+    });
   });
 });
-//small addition
